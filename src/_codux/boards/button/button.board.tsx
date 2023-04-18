5bede75e7550b67e2dfbbf85f98ed8b9e4e5c232
Konrad Dębiec <konradd@tutanota.com>
2023-04-18 23:33:23 +0200

    Removed codux

--- src/_codux/boards/button/button.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import { Button } from '../../../components/button/button';
-
-export default createBoard({
-    name: 'Button',
-    Board: () => <Button>
-    </Button>
-});
